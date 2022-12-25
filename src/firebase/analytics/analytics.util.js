24b3ded09b155ada0fcafdecde7770d6e2af7306
IshanSingla <is.ishan.singla@gmail.com>
2022-12-25 13:21:03 +0530

    sixth commit

--- src/firebase/analytics/analytics.util.js
+++ src/firebase/analytics/analytics.util.js
@@ -1,7 +1,13 @@
-// import { logEvent } from "firebase/analytics";
-// import { analytics } from "../firebase.util";
+import {
+  logEvent,
+  setUserProperties,
+  setCurrentScreen,
+  setUserId,
+} from "firebase/analytics";
+import { analytics } from "../firebase.util";
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import Axios from "axios";
 
 // logEvent(analytics, "select_content", {
 //   content_type: "image",
@@ -17,16 +23,28 @@ export const FirebaseAnalytics = () => {
     window.gtag("event", "page_view", {
       page_path: location.pathname + location.search,
     });
-    const analytics = window.firebase && window.firebase.analytics;
-    if (typeof analytics === "function") {
-      const page_path = location.pathname + location.search;
-      analytics().setCurrentScreen(page_path);
-
-      analytics().logEvent("page_view", {
-        page_path,
-        page_location: window.location.href,
+    const page_path = location.pathname + location.search;
+    setCurrentScreen(analytics, page_path);
+    Axios.get("https://ipapi.co/json/").then((res) => {
+      setUserId(analytics, res.data.ip);
+      setUserProperties(analytics, {
+        ip: res.data.ip,
+        country: res.data.country_name,
+        city: res.data.city,
+        region: res.data.region,
+        postal: res.data.postal,
+        timezone: res.data.timezone,
+        latitude: res.data.latitude,
+        longitude: res.data.longitude,
+        
       });
-    }
+      logEvent(analytics, "view", {
+        path: window.location.href,
+        route: page_path,
+        ip: res.data.ip,
+      });
+    });
+    // }
   }, [location]);
   return null;
 };
